eb57b3ab60f46472f50aad7a42a3a7bfafca8dc2
Amul Patel <amulp84@gmail.com>
2013-12-16 04:02:57 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backbonish::Application.config.secret_key_base = '6cae8c54a47f4a3f2432074e2ad02b19cc855c4747f731aa2380db4b37cafa929a00349d51e6fcc96bfd4913fe137f386a135f87cc18bd657b58970327243b36'
